9e2f921c6d23fd2c5f17d58d8f3aca60bbabb868
ADNath <dawaking8@gmail.com>
2026-07-06 10:54:00 +0600

    image move to public

--- website/src/features/about/data/topManagement.ts
+++ website/src/features/about/data/topManagement.ts
@@ -11,7 +11,7 @@ export const topManagement: ManagementMember[] = [
     id: 1,
     name: "MR. MANZURUL HASSAN",
     designation: "Chairman",
-    image: "/assets/images/management/chairman.png",
+    image: "/images/management/chairman.png",
     message:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
@@ -19,7 +19,7 @@ export const topManagement: ManagementMember[] = [
     id: 2,
     name: "MR. MAHBUBUL HASAN",
     designation: "Managing Director",
-    image: "/assets/images/management/md.jpeg",
+    image: "/images/management/md.jpeg",
     message:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
@@ -27,7 +27,7 @@ export const topManagement: ManagementMember[] = [
     id: 3,
     name: "TARIK MD MAHABUBUL ISLAM",
     designation: "Executeive Director",
-    image: "/assets/images/management/ed.jpeg",
+    image: "/images/management/ed.jpeg",
     message:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
